151755fdd6cda6c21df5161b410c1c07877adcb4
wani <wani@prnd.co.kr>
2025-04-14 16:41:32 +0900

    HDI-12838   [라이브러리] Stinsen 종종 parent 못찾는 현상 개선

--- Sources/IntroSpection/IntrospectionUIViewController.swift
+++ Sources/IntroSpection/IntrospectionUIViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 @available(iOS 13.0, tvOS 13.0, macOS 10.15.0, *)
 class IntrospectionUIViewController: UIViewController {
+    
+    var handler: ((IntrospectionUIViewController) -> Void)? = nil
+
     required init() {
         super.init(nibName: nil, bundle: nil)
         view = IntrospectionUIView()
@@ -18,6 +21,26 @@ class IntrospectionUIViewController: UIViewController {
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        handler?(self)
+    }
+
+    override func didMove(toParent parent: UIViewController?) {
+        super.didMove(toParent: parent)
+        handler?(self)
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        handler?(self)
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        handler?(self)
     }
 }
 #endif
